e82959fcc29f4e112b442a4d22f8ef2e48cba8e8
khleon <50057841+khleon@users.noreply.github.com>
2019-10-17 22:59:17 +0300

    new

--- Calculate.java
+++ Calculate.java
@@ -1,8 +1,10 @@
 public class Calculate{
 	public static void main(String[] arg){
 		System.out.println("Calculating...");
-		
-		
+		double first = Double.valueOf(arg[0]);
+		double second = Double.valueOf(arg[1]);
+		double summ = first + second;
+		System.out.println("summ " + summ);
 	}
 		
 }
